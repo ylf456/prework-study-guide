eecf0ef87f482ee2f251f539e01a399144808513
ylf456 <145236890+ylf456@users.noreply.github.com>
2023-09-23 22:04:22 -0700

    add-js to the project

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+var topic = "HTML";
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for (var x=0; x<shapes.length;x+=1) {console.log(shapes[x]);}
+
